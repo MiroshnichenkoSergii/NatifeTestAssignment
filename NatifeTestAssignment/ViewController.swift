51de97adb6147e6045949b6bc086eb261602be54
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2022-09-26 13:28:13 +0300

    Initial Commit

---
+++ NatifeTestAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeTestAssignment
+//
+//  Created by Sergii Miroshnichenko on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
